61d4ed1c5c5934b79a85c73b3c56a83ac108bd01
André Brito <felipe.brito016@gmail.com>
2021-12-18 15:46:02 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = "radarbot-team"
+}
+
+resource "github_repository" "test-terraform" {
+  name = "test-terraform"
+  description = "Test Terraform Repository"
+  visibility = "public"
+}
+
+resource "github_repository" "terraform-radarbot-team" {
+  name = "terraform-radarbot-team"
+  description = "Test Terraform Repository"
+  visibility = "public"
+}
