e53f374207fb5cebcb9640baa221878f39d48ac5
André Brito <felipe.brito016@gmail.com>
2021-12-19 19:40:54 -0400

    remove duplicate of all orgs members

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-resource "github_team" "all_org_members" {
-  name        = "all-org-members"
-  description = "All members of the RadarBot Team"
-  privacy     = "closed"
-}
-
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
   username = ""
