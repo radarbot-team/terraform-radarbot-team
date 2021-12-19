c99cb0478ad8cb97fc0d7eeef30a88a20b4070f2
André Brito <felipe.brito016@gmail.com>
2021-12-19 00:57:10 -0400

    import repositories

--- main.tf
+++ main.tf
@@ -15,4 +15,19 @@ resource "github_repository" "terraform-radarbot-team" {
   name        = "terraform-radarbot-team"
   description = "Test Terraform Repository"
   visibility  = "public"
+
+}
+
+resource "github_branch_protection_v3" "protection-master" {
+  repository = github_repository.terraform-radarbot-team
+  branch     = "master"
+
+  required_pull_request_reviews {
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_repository" "test-terraform" {
+  name        = "test-terraform"
+  description = "Test Terraform Repository"
 }
