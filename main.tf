3ad3e41e8dbe656bc7b93d7a0482e4d97608acb3
André Brito <felipe.brito016@gmail.com>
2021-12-19 19:56:00 -0400

    rename file to fix error on forEach

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
   username = ""
