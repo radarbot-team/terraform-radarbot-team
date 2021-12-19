902cd07b9078d478dd7a53210303a9e91d1a6527
André Brito <felipe.brito016@gmail.com>
2021-12-19 14:03:38 -0400

    add team_id

--- org/org_admin_members.tf
+++ org/org_admin_members.tf
@@ -2,10 +2,12 @@ module "afbb_0987" {
   source      = "../modules/org-member"
   discord_tag = "afbb#0987"
   role        = "admin"
+  team_id     = data.github_team.all-org-members.id
 }
 
 module "afbb_0988" {
   source      = "../modules/org-member"
   discord_tag = "afbb#0988"
   role        = "admin"
+  team_id     = data.github_team.all-org-members.id
 }
