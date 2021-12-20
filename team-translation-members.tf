d9be3d7cae22d753911fdec551bb8fadd33a7e3a
André Brito <felipe.brito016@gmail.com>
2021-12-20 10:38:22 -0400

    remove member from team-translation

--- team-translation-members.tf
+++ team-translation-members.tf
@@ -1,9 +1,3 @@
-resource "github_team_membership" "onxyj3_translation_team" {
-  team_id  = github_team.translation_team_writer.id
-  username = "onyxj3"
-  role     = "member"
-}
-
 resource "github_team_membership" "indekt_translation_team" {
   team_id  = github_team.translation_team_writer.id
   username = "indekt"
