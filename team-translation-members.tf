5bdea0cf14bce36e5718585070f180dacfbcfd7d
André Brito <felipe.brito016@gmail.com>
2021-12-20 10:18:10 -0400

    add indekt to translation team

--- team-translation-members.tf
+++ team-translation-members.tf
@@ -3,3 +3,11 @@ resource "github_team_membership" "onxyj3_translation_team" {
   username = "onyxj3"
   role     = "member"
 }
+
+resource "github_team_membership" "indekt_translation_team" {
+  team_id  = github_team.translation_team_writer.id
+  username = "indekt"
+  role     = "member"
+}
+
+
