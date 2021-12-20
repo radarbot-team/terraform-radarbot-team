e46670ded208c004094e03b3d9fb2472e5202dd7
Indekt <70900981+indekt@users.noreply.github.com>
2021-12-20 13:44:19 +0100

    Update user Translation Team Group

--- team-translation-members.tf
+++ team-translation-members.tf
@@ -4,7 +4,7 @@ resource "github_team_membership" "onxyj3_translation_team" {
   role     = "member"
 }
 
-resource "github_team_membership" "onxyj3_translation_team" {
+resource "github_team_membership" "indekt_translation_team" {
   team_id  = github_team.translation_team_writer.id
   username = "indekt"
   role     = "member"
