ebc9706817a45d16c596684aec1b6ca04389ffc3
André Brito <felipe.brito016@gmail.com>
2022-01-17 12:11:48 -0400

    fix module name

--- team-translation-members.tf
+++ team-translation-members.tf
@@ -4,7 +4,7 @@ resource "github_team_membership" "indekt_translation_team" {
   role     = "member"
 }
 
-resource "github_team_membership" "indekt_translation_team" {
+resource "github_team_membership" "enrico1108_translation_team" {
   team_id  = github_team.translation_team_writer.id
   username = "enrico1108"
   role     = "member"
